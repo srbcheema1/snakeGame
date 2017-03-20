8995219ec7db5409a0f2f3548bc73ebabcf9b124
Sarbjit Singh <srbcheema1@gmail.com>
2017-03-20 19:08:32 +0530

    initial commit

---
+++ src/snakegame/SnakeGame.java
@@ -0,0 +1,10 @@
+package snakegame;
+
+public class SnakeGame {
+    
+    public static void main(String[] args) {
+        MainFrame mainframe = new MainFrame();
+        mainframe.setVisible(true); 
+    }
+    
+}
